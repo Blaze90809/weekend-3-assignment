0b4454a1862a57388e5ac68d040b808dab6d69ec
Blaze Fugina <blaze.fugina@gmail.com>
2017-10-20 18:43:17 -0500

    Working on post route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,12 +4,12 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('JQ ready');
     getTasks();
-    $('#submit').on('click', appendDom);
+    $('#submit').on('click', postList);
 }
 
 
 function getTasks(){
-console.log('in tasks');
+console.log('in tasks'); // OG get route to get list
 $.ajax({
     type: 'GET',
     url: '/getlist'
@@ -20,7 +20,25 @@ $.ajax({
 }).fail(function(error){
     console.log('GET error', error)
 })
-}
+} // end of OG get route
+
+function postList(){
+  var listItem = $('#enterTask').val();
+  sendList = {
+    task : listItem,
+    completed: false,
+  } 
+  $.ajax({
+      type: 'POST', // post route to add list items
+      url: 'postlist/',
+      data: sendList
+  }).done(function(response){
+     console.log('sending this:', sendList);
+
+  }).fail(function(error){
+      console.log('POST error', error);
+  })
+  } // end of POST route
 
 function appendDom(){
     console.log('append DOM time');
